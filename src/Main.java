07390e0400281f0624a809c26d74d9fd838fd78d
aldojunior990 <aldojunior990@gmail.com>
2023-12-07 20:39:16 -0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,6 @@ public class Main {
     public static void main(String[] args) {
         ThreadSemaphoreFactory threadFactory = new ThreadSemaphoreFactory();
 
-        // 0 -> Vermelho
-        // 1 -> Verde
-        // 2 -> Amarelo
-
         Thread first_semaphore = threadFactory.createThread("semaforo 1", Colors.GREEN);
         Thread second_semaphore = threadFactory.createThread("semaforo 2", Colors.RED);
 
--- src/Semaphore.java
+++ src/Semaphore.java
@@ -4,11 +4,18 @@ import static java.lang.Thread.sleep;
 public class Semaphore implements Runnable {
 
     private final String _name;
+
+    private Integer _timeDelay;
     private Colors _startsWith;
 
     public Semaphore(String name, Colors startsWith) {
         _name = name;
+        _timeDelay = 5000;
         _startsWith = startsWith;
+    }
+
+    public void setTimeDelay(Integer timeDelay) {
+        _timeDelay = timeDelay;
     }
 
     private void toSleep(Integer time) {
@@ -21,7 +28,7 @@ public class Semaphore implements Runnable {
 
     private void toGreenState() {
         System.out.println("[" + _name + "]" + " Muda pra VERDE");
-        toSleep(4000);
+        toSleep((int) (_timeDelay - (_timeDelay * 0.8)));
         _startsWith = Colors.YELLOW;
     }
 
@@ -33,7 +40,7 @@ public class Semaphore implements Runnable {
 
     private void toYellowState() {
         System.out.println("========" + "[" + _name + " Muda pra AMARELO " + "]" + "========");
-        toSleep(1000);
+        toSleep((int) (_timeDelay * 0.8));
         _startsWith = Colors.RED;
     }
 
